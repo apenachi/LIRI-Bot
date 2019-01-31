298b9ac6d22a62955258c877dab968271824d4da
JVGH <17502830+JVGH@users.noreply.github.com>
2019-01-31 14:00:08 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID || null,
-  secret: process.env.SPOTIFY_SECRET || null
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
 exports.omdb = process.env.OMDB_KEY;
